8498c66c1d989f5567cac3ca28f49cb563fe999a
barbaros <barbarosayildirim@gmail.com>
2023-05-16 18:36:07 +0300

    feat: update all logic

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,171 @@
-console.log('Try npm run lint/fix!');
+import * as fs from 'fs';
+import * as readline from 'readline';
 
-const longString =
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut aliquet diam.';
+const board: number[][] = Array.from({length: 10}, () => Array(10).fill(0));
+let turnPlayer = 1;
 
-const trailing = 'Semicolon';
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-const why = 'am I tabbed?';
-
-export function doSomeStuff(
-  withThis: string,
-  andThat: string,
-  andThose: string[]
-) {
-  //function on one line
-  if (!andThose.length) {
-    return false;
-  }
-  console.log(withThis);
-  console.log(andThat);
-  console.dir(andThose);
-  return;
+function logFile(column: number): void {
+  fs.appendFileSync(
+    'log.txt',
+    `${turnPlayer}. Oyuncu ${column} sütununa yerleştirdi.\n`
+  );
 }
-// TODO: more examples
+
+function printBoard(): void {
+  for (let i = 9; i >= 1; i--) {
+    let row = '';
+    for (let j = 1; j <= 9; j++) {
+      row += board[i][j] + ' ';
+    }
+    console.log(row);
+  }
+}
+
+function checkGame(): number {
+  for (let i = 1; i <= 9; i++) {
+    for (let j = 1; j <= 9; j++) {
+      if (i + 3 <= 9 && j + 3 <= 9) {
+        if (
+          board[i][j] !== 0 &&
+          board[i][j] === board[i + 1][j + 1] &&
+          board[i][j] === board[i + 2][j + 2] &&
+          board[i][j] === board[i + 3][j + 3]
+        ) {
+          return board[i][j];
+        }
+        if (
+          board[i][j] !== 0 &&
+          board[i][j] === board[i + 1][j - 1] &&
+          board[i][j] === board[i + 2][j - 2] &&
+          board[i][j] === board[i + 3][j - 3]
+        ) {
+          return board[i][j];
+        }
+      }
+      if (j + 3 <= 9) {
+        if (
+          board[i][j] !== 0 &&
+          board[i][j] === board[i][j + 1] &&
+          board[i][j] === board[i][j + 2] &&
+          board[i][j] === board[i][j + 3]
+        ) {
+          return board[i][j];
+        }
+      }
+      if (i + 3 <= 9) {
+        if (
+          board[i][j] !== 0 &&
+          board[i][j] === board[i + 1][j] &&
+          board[i][j] === board[i + 2][j] &&
+          board[i][j] === board[i + 3][j]
+        ) {
+          return board[i][j];
+        }
+      }
+    }
+  }
+  return 0;
+}
+
+async function addToken(): Promise<void> {
+  const input = await new Promise<number>(resolve => {
+    rl.question('', (answer: string) => {
+      resolve(parseInt(answer));
+    });
+  });
+
+  if (input === 0) {
+    return gameMenu();
+  }
+
+  board[board[0][input] + 1][input] = turnPlayer;
+  board[0][input] += 1;
+  logFile(input);
+  printBoard();
+  const gameStatus = checkGame();
+  if (gameStatus !== 0) {
+    console.log(`Oyunu kazanan: ${gameStatus}`);
+    return;
+  }
+  turnPlayer = turnPlayer === 1 ? 2 : 1;
+  return playLoop();
+}
+
+async function playLoop(): Promise<void> {
+  if (turnPlayer === 1) {
+    console.log('Ana menü için 0 yazabilirsin.');
+    console.log('1. Oyuncu: Hangi sütuna yerleştirmek istiyorsun?');
+    return addToken();
+  } else if (turnPlayer === 2) {
+    console.log('Ana menü için 0 yazabilirsin.');
+    console.log('2. Oyuncu: Hangi sütuna yerleştirmek istiyorsun?');
+    return addToken();
+  }
+}
+
+function saveBoard() {
+  let data = '';
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      data += board[i][j] + ' ';
+    }
+    data += '\n';
+  }
+  data += turnPlayer + '\n';
+  fs.writeFileSync('game_data.txt', data);
+  console.log('Oyun kaydedildi.');
+}
+
+function loadBoard() {
+  const data = fs.readFileSync('game_data.txt', 'utf8');
+  const lines = data.split('\n');
+  for (let i = 0; i < 10; i++) {
+    const row = lines[i].split(' ').map((x: string) => parseInt(x));
+    for (let j = 0; j < 10; j++) {
+      board[i][j] = row[j];
+    }
+  }
+  turnPlayer = parseInt(lines[10]);
+  console.log('Oyun yüklendi.');
+}
+
+async function gameMenu(): Promise<void> {
+  console.log('1-Oyuna Başla');
+  console.log('2-Oyunu Kaydet');
+  console.log('3-Oyunu Yükle');
+  console.log('4-Oyundan Çık');
+
+  const gameChoice = await new Promise<number>(resolve => {
+    rl.question('', (answer: string) => {
+      resolve(parseInt(answer));
+    });
+  });
+
+  if (gameChoice === 1) {
+    printBoard();
+    return playLoop();
+  } else if (gameChoice === 2) {
+    saveBoard();
+    return gameMenu();
+  } else if (gameChoice === 3) {
+    loadBoard();
+    return gameMenu();
+  } else if (gameChoice === 4) {
+    rl.close();
+    process.exit(0);
+  } else {
+    return gameMenu();
+  }
+}
+
+async function main() {
+  await gameMenu();
+  process.exit(0);
+}
+
+main();
